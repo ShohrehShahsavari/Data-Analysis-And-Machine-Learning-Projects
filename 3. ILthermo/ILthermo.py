1c4fb18beb2d6b72f0b2ad0d792e034634ffc24c
Shohreh Shahsavari <Shohre.shahsavari@gmail.com>
2025-04-25 16:31:18 +0330

    update folder

--- 3. ILthermo/ILthermo.py
+++
@@ -1,6 +0,0 @@
-import ilthermopy as il
-import pandas as pd
-
-
-il.Search(compound='',n_compounds=[1,2,3],prop='',prop_key='')
-#(compound: Optional[str] = None, n_compounds: Optional[typing_extensions.Literal[None, 1, 2, 3]] = None, prop: Optional[str] = None, prop_key: Optional[str] = None, year: Optional[int] = None, author: Optional[str] = None, keywords: Optional[str] = None)
